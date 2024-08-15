4dfcd936ce43e7e6da889d5dd0c9647e9c1ed172
abrahamjust <162578329+abrahamjust@users.noreply.github.com>
2024-08-15 06:20:20 +0000

    completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(str, num) {
     returnStr = '';
+    if(num < 0) {
+        return 'ERROR';
+    }
     for(let i = 0; i < num; i++) {
         returnStr += str;
     }
