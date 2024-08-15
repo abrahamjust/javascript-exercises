5d6b0d46ec29b67e5f29030fe42e71abf67bac91
abrahamjust <162578329+abrahamjust@users.noreply.github.com>
2024-08-15 06:17:46 +0000

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reverseStr = '';
+    for(let i = str.length-1; i >= 0; i--) {
+        let c = str.charAt(i);
+        reverseStr += c;
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
