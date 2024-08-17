9de007540236ae713ac9e7b6a2abd0c53d0ef711
abrahamjust <162578329+abrahamjust@users.noreply.github.com>
2024-08-17 11:45:51 +0000

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...val) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        //...val is an array, so u can use .includes() to check if the value is there in the parameters or not
+        if(!val.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
